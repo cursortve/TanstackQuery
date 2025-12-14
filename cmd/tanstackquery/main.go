da37696db24c7ac6264eb44ac08bed4000b4b1a8
Tie <125909656+astabrutie8@users.noreply.github.com>
2025-12-15 00:22:48 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/tanstackquery/main.go
@@ -0,0 +1,23 @@
+// cmd/tanstackquery/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "tanstackquery/internal/tanstackquery"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := tanstackquery.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
